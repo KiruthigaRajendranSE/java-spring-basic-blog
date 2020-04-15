bdcbe22427d187329b62e9912fc075c0a37a972f
KiruthigaRajendranSE <62032172+KiruthigaRajendranSE@users.noreply.github.com>
2020-04-14 22:38:23 -0700

    Added a ModelMap parameter

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,16 +1,15 @@
 package com.pluralsight.blog;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController {
 
-	@RequestMapping("/home")
-	public String listPosts(Map ModelMap) {
-		ModelMap.put("title", "Blog Post 1");
+	@RequestMapping("/")
+	public String listPosts(ModelMap modelmap) {
+		modelmap.put("title", "Blog Post 1");
 		return "home";
 	}
 }
